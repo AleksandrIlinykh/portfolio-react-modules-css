3284f7ad4870535bab82031b0ef96afa8ea88c71
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-17 11:28:35 +0500

    added App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,19 @@
 import appStyles from "./app.module.css";
 import Contacts from "../contacts/contacts";
-
+import Header from "../header/header";
+import Home from "../home/home";
 function App() {
   return (
     <>
       <div className={appStyles.app}>
-        <div className={appStyles.section}>
-          <Contacts />
+        <Header />
+        <div className={appStyles.sections}>
+          <div className={appStyles.section}>
+            <Home />
+          </div>
+          <div className={appStyles.section}>
+            <Contacts />
+          </div>
         </div>
       </div>
     </>
