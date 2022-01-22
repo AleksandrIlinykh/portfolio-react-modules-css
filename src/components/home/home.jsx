dfc11d89ce392408e54f3bad9f8e57fac3b40573
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-22 13:04:53 +0500

    Restyled Home - scroll animation and CV

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,28 +1,36 @@
 import "./home.css";
+import homeStyles from "./home.module.css";
 import myPhoto from "../../images/myPhoto.png";
 import arrowDown from "../../images/arrow-down.png";
-
+import cv from "../../files/cv-alexander-ilinykh.pdf";
 export default function Home() {
   return (
     <>
-      <div className="home" id="home">
-        <div className="imgContainer">
+      <div className={homeStyles.home} id="home">
+        <div className={homeStyles.imgContainer}>
           <img src={myPhoto} alt="" />
         </div>
 
-        <div className="aboutTextContainer">
+        <div className={homeStyles.aboutTextContainer}>
           <h1>
             Hi! <br />
             I'm Alexandr <br />a WEB developer <br />
           </h1>
           <p>This is my self-presentation page</p>
           <p>Scroll down to know more about me</p>
+          <p>
+            <a href={cv} download target="_blank">
+              Or just download my CV
+            </a>
+          </p>
         </div>
 
-        <a href="#portfolio" className="left">
+        <a href="#portfolio" className={homeStyles.left}>
+          <p>Scroll!</p>
           <img src={arrowDown} alt="" />
         </a>
-        <a href="#portfolio" className="right">
+        <a href="#portfolio" className={homeStyles.right}>
+          <p>Scroll!</p>
           <img src={arrowDown} alt="" />
         </a>
       </div>
